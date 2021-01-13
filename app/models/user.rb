503905f31b80b6b7f330b95d54d7bbbd819a9ec8
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-01-13 18:19:43 +0200

    user validations & age scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/.freeze
+  has_many :posts
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: {with: VALID_EMAIL}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 8, maximum: 15 }
+
+  scope :adults, -> { where('birthday <= ?', 18.years.from_now) }
+
+
+
+
 end
