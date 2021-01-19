a988980f64bc3719b98c7d4d9a41b63cd9b65a21
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-01-20 01:27:04 +0200

    membership validation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,8 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :user, uniqueness: { scope: :group }, presence: true
+  validates :group, presence: true
+
 end
