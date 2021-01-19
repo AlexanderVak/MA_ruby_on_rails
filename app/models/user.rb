7122e2380e1b44d93f2c64b643a5a7e2f80c8dd8
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-01-19 21:43:35 +0200

    added many-to-many connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/.freeze
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :memberships
+  has_many :groups, through: :memberships
   has_many :images, as: :imageable
 
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, format: {with: VALID_EMAIL}, uniqueness: true
-  validates :password, presence: true, length: {minimum: 8, maximum: 15 }
-
   scope :adults, -> { where('birthday <= ?', 18.years.from_now) }
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 15 }
 
   def full_name
     "#{first_name} #{last_name}"
