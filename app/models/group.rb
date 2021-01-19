521f482f2cd749aacfe097a05d6c085528494a52
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-01-19 21:58:14 +0200

    validation & image association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,9 @@
 class Group < ApplicationRecord
   has_many  :memberships, dependent: :destroy
   has_many :users, through: :memberships
+  has_many :images, as: :imageable
+
+  validates :name, presence: true, uniqueness: true
+  validates :group_type, :status, presence: true
 
 end
