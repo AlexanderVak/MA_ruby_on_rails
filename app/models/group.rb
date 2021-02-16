dad251b55de432503e022b4ce57d76b305db3b0d
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-02-16 19:19:18 +0200

    add dependent destroy to image

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many  :memberships, dependent: :destroy
   has_many :users, through: :memberships
-  has_many :images, as: :imageable
+  has_many :images, as: :imageable, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :group_type, :status, presence: true
